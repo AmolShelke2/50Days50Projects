de9796e0436d0127beec0e0d51eede3a55ee7f94
Amol shelke <shelkeamol897@gmail.com>
2022-01-09 22:22:38 +0530

    update the winning score

--- Pig Game/script.js
+++ Pig Game/script.js
@@ -57,7 +57,7 @@ document.querySelector(".btn--hold").addEventListener("click", function () {
     scores[activePlayer];
 
   // check if player won the Game
-  if (scores[activePlayer] >= 20) {
+  if (scores[activePlayer] >= 100) {
     document.querySelector("#name--" + activePlayer).textContent = "Winner❤️";
     document.querySelector(".dice").style.display = "none";
     document
