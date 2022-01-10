715f89d8f8c776c9e8b13880bec15ea50ff2c09e
Amol shelke <shelkeamol897@gmail.com>
2022-01-10 13:07:04 +0530

    increase the max-score

--- Pig Game/script.js
+++ Pig Game/script.js
@@ -57,7 +57,7 @@ document.querySelector(".btn--hold").addEventListener("click", function () {
       scores[activePlayer];
 
     // check if player won the Game
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       // state variable
 
       isPlaying = false;
