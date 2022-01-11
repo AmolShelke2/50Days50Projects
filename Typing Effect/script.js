26594e35e3887066314411149959b8df1506a348
Amol shelke <shelkeamol897@gmail.com>
2022-01-11 22:27:38 +0530

    text changed

--- Typing Effect/script.js
+++ Typing Effect/script.js
@@ -1,6 +1,6 @@
 const textEl = document.getElementById("text");
 const speedEl = document.getElementById("speed");
-const text = "Hey EveryOne I am Amol Shelke Front End Dev";
+const text = "Hey EveryOne I am Amol Shelke Front End Dev🤙";
 
 let idx = 1;
 let speed = 300 / speedEl.value;
