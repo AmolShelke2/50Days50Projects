120d62105623d6795938aaeb37dc7e009f18e11a
Amol shelke <shelkeamol897@gmail.com>
2022-01-11 20:46:46 +0530

    Done

--- Typing Effect/script.js
+++ Typing Effect/script.js
@@ -9,4 +9,15 @@ writeText();
 
 function writeText() {
   textEl.innerHTML = text.slice(0, idx);
+  idx++;
+
+  if (idx > text.length) {
+    idx = 1;
+  }
+
+  setTimeout(writeText, speed);
 }
+
+speedEl.addEventListener("input", (e) => {
+  speed = 300 / e.target.value;
+});
