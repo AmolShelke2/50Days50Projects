5f7418e7331ffb9bb02c62cf327aede91569f47c
Amol shelke <shelkeamol897@gmail.com>
2022-01-11 20:40:12 +0530

    update style

---
+++ Typing Effect/script.js
@@ -0,0 +1,8 @@
+const textEl = document.getElementById("text");
+const speedEl = document.getElementById("speed");
+const text = "Hey EveryOne I am Amol Shelke💕";
+
+let idx = 1;
+let speed = 300 / speedEl.value;
+
+writeText();
