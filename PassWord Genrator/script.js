a905a0eaf1f96454d47c3204f7d27398f7aeccee
Amol shelke <shelkeamol897@gmail.com>
2022-01-13 15:57:41 +0530

    aded files

---
+++ PassWord Genrator/script.js
@@ -0,0 +1,17 @@
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+  const symbols = "";
+}
+
+console.log(getRandomNumber());
