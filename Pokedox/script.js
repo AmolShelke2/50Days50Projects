a2825e25ad67774e49cb3c8b8b266fb76d085209
Amol shelke <shelkeamol897@gmail.com>
2022-01-19 21:44:15 +0530

    update script.js

--- Pokedox/script.js
+++ Pokedox/script.js
@@ -51,7 +51,7 @@ const createPokemonCard = function (pokemon) {
 
   const pokemonInnerHTML = ` 
     <div class="img-container">
-       <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"" 
+       <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"
        alt="${name}">
         </div>
         <div class="info">
