62d96e026be54229d08ef502c5eb8ea8f720621f
Amol shelke <shelkeamol897@gmail.com>
2022-01-22 12:45:54 +0530

    updated script.js

--- TodoAPP/script.js
+++ TodoAPP/script.js
@@ -2,6 +2,12 @@ const form = document.getElementById("form");
 const input = document.getElementById("input");
 const todosUl = document.getElementById("todos");
 
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+if (todos) {
+  todos.forEach((todo) => addTodo(todo));
+}
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -23,18 +29,37 @@ function addTodo(todo) {
 
     todoEl.innerText = todoText;
 
-    todoEl.addEventListener("click", () =>
-      todoEl.classList.toggle("completed")
-    );
+    todoEl.addEventListener("click", () => {
+      todoEl.classList.toggle("completed");
+      UpdateLS();
+    });
 
     todoEl.addEventListener("contextmenu", (e) => {
       e.preventDefault();
 
       todoEl.remove();
+      UpdateLS();
     });
 
     todosUl.appendChild(todoEl);
 
     input.value = "";
+
+    UpdateLS();
   }
+}
+
+function UpdateLS() {
+  todoEl = document.querySelectorAll("li");
+
+  const todos = [];
+
+  todoEl.forEach((todoEl) => {
+    todos.push({
+      text: todoEl.innerText,
+      completed: todoEl.classList.contains("completed"),
+    });
+  });
+
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
