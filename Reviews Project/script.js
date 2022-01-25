262252a408348ba0b4fbbc17884ef3a1d72bea79
Amol shelke <shelkeamol897@gmail.com>
2022-01-25 12:34:34 +0530

    update script.js

--- Reviews Project/script.js
+++ Reviews Project/script.js
@@ -31,3 +31,54 @@ const reviews = [
 
 const img = document.getElementById("person-img");
 const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
+
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const randomBtn = document.querySelector(".random-btn");
+
+let currentItem = 0;
+
+// load initial item
+window.addEventListener("DOMContentLoaded", function () {
+  showPerson();
+});
+
+// show Person based on item
+
+function showPerson() {
+  const item = reviews[currentItem];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
+
+// show next person
+
+nextBtn.addEventListener("click", function () {
+  currentItem++;
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
+  }
+  showPerson();
+});
+
+prevBtn.addEventListener("click", function () {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
+  showPerson();
+});
+
+// Challenge  create random button and show the random review
+// each time when user click on the surprise me button
+
+randomBtn.addEventListener("click", function () {
+  let randomItem = Math.floor(Math.random() * reviews.length);
+  currentItem = randomItem;
+
+  showPerson();
+});
