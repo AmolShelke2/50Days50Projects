4e0a39a15cf7f1ea8d3ba26fb32bed57602886dd
Amol shelke <shelkeamol897@gmail.com>
2022-01-30 16:21:16 +0530

    modified script.js

--- NavBar/script.js
+++ NavBar/script.js
@@ -2,5 +2,9 @@ const navToggle = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
 
 navToggle.addEventListener("click", function () {
-  links.classList.toggle("show-links");
+  if (links.classList.contains("show-links")) {
+    links.classList.remove("show-links");
+  } else {
+    links.classList.add("show-links");
+  }
 });
