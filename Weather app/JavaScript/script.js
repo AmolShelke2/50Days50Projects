5ba91f541c3eeeecfab83efa2ce413056383707b
Amol shelke <shelkeamol897@gmail.com>
2022-02-03 13:17:07 +0530

    update files

---
+++ Weather app/JavaScript/script.js
@@ -0,0 +1,32 @@
+const weatherApi = {
+  key: "bab281d79e5f1e9755a68d754cc313e7",
+  baseUrl: "https://api.openweathermap.org/data/2.5/weather",
+};
+
+const getBtn = document.getElementById("get-btn");
+const inputBox = document.getElementById("input-box");
+const weatherBODY = document.querySelector(".weather-body");
+getBtn.addEventListener("click", async () => {
+  const input = inputBox.value;
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?appid=bab281d79e5f1e9755a68d754cc313e7&q=${input}&units=metric`
+  );
+  const weather = await response.json();
+  console.log(weather);
+
+  const cardHTML = `
+        <div class="location-details">
+          <div class="city" id="city">${weather.name}</div>
+          <div class="date" id="date">3 Feb (ThursDay), 2022</div>
+        </div>
+
+        <div class="weather-status">
+          <div class="temp" id="temp">${weather.main.temp}&deg;Cs</div>
+          <div class="min-max" id="min-max">
+          ${weather.main.temp_min} / ${weather.main.temp_max}&deg;C (max)
+          </div>
+        </div>
+  `;
+  weatherBODY.style.display = "block";
+  weatherBODY.innerHTML = cardHTML;
+});
--- Weather app/script.js
+++
@@ -1,4 +0,0 @@
-const weatherApi = {
-  key: "bab281d79e5f1e9755a68d754cc313e7",
-  baseUrl: "https://api.openweathermap.org/data/2.5/weather",
-};
