4490ed56968a3850de91cc200451c5307d43bf31
Amol shelke <shelkeamol897@gmail.com>
2022-02-07 13:18:29 +0530

    update script.js

---
+++ Movie Suggestor/script.js
@@ -0,0 +1,18 @@
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function (question) {
+  const btn = question.querySelector(".question-btn");
+  // console.log(btn);
+
+  btn.addEventListener("click", function () {
+    // console.log(question);
+
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+
+    question.classList.toggle("show-text");
+  });
+});
