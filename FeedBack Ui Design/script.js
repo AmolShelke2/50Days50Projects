54112c0cffe96d80b7eb64515f19b6314723b468
Amol shelke <shelkeamol897@gmail.com>
2022-05-10 18:56:55 +0530

    Fixed feedback error

--- FeedBack Ui Design/script.js
+++ FeedBack Ui Design/script.js
@@ -4,7 +4,7 @@ const sendBtn = document.querySelector("#send");
 const panel = document.querySelector("#panel");
 let selectedRating = "Satisfied";
 
-ratingsContainer.addEventListener("click", function (e) {
+ratingsContainer.addEventListener("click", (e) => {
   if (
     e.target.parentNode.classList.contains("rating") &&
     e.target.nextElementSibling
@@ -14,7 +14,7 @@ ratingsContainer.addEventListener("click", function (e) {
     selectedRating = e.target.nextElementSibling.innerHTML;
   } else if (
     e.target.parentNode.classList.contains("rating") &&
-    e.parentNode.previousSibling &&
+    e.target.previousSibling &&
     e.target.previousElementSibling.nodeName === "IMG"
   ) {
     removeActive();
@@ -23,19 +23,18 @@ ratingsContainer.addEventListener("click", function (e) {
   }
 });
 
-sendBtn.addEventListener("click", function (e) {
+sendBtn.addEventListener("click", (e) => {
   panel.innerHTML = `
-        <i class='fas fa-heart'></i>
-            <strong>Thank You</strong>
-        <br>
-
-        <strong>FeedBack: ${selectedRating}</strong>
-        <p>We'll Use your feedback to improve our customer support</p>
-    `;
+  <i class="fas fa-heart"></i>
+  <strong>Thank You!</strong>
+  <br>
+  <strong>Feedback: ${selectedRating}</strong>
+  <p>We'll use your feedback to improve our customer support</p>
+  `;
 });
 
 function removeActive() {
   for (let i = 0; i < ratings.length; i++) {
-    ratings[0].classList.remove("active");
+    ratings[i].classList.remove("active");
   }
 }
